b8b21c701eb7fe0e4eb496c7ac8a8ee1747af862
babayotta <babayotta@protonmail.ch>
2020-01-27 19:52:40 +0300

    Function model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Function(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    function_text = models.CharField(max_length=200)
+    interval = models.FloatField()
+    step = models.FloatField()
+    graph = models.ImageField()
+    date = models.DateTimeField()
